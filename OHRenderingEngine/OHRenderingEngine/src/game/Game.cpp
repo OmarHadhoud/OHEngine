087f98e6e8c21018fb7b1d99aabbd87e87bd4973
Omar Salem Hadhoud <omarhadhoud@ymail.com>
2020-09-12 11:34:39 +0200

    Finished a simple scene with simple motion blur, borders for model, skybox and simple light manager

--- OHRenderingEngine/OHRenderingEngine/src/game/Game.cpp
+++ OHRenderingEngine/OHRenderingEngine/src/game/Game.cpp
@@ -40,11 +40,8 @@ bool Game::GameEnded()
 
 int Game::RunLevel()
 {
-	m_Moving = false;
+	//Initialize the level details
 	m_Renderer.SetActiveWindow(m_CurrentWindow);
-	
-	
-
 	std::vector<std::string> cubeMapPaths = {
 		"res/textures/px.png",
 		"res/textures/nx.png",
@@ -55,7 +52,10 @@ int Game::RunLevel()
 	};
 	m_Skybox = std::make_unique<Skybox>(cubeMapPaths);
 	m_LightManager = std::make_unique<LightManager>();
-
+	VertexBufferLayout TempVBL;
+	int glfwWidth;
+	int glfwHeight;
+	m_MovingSpeed = 0;
 
 	//Post procesing data
 	float quadVertices[] = 
@@ -68,57 +68,35 @@ int Game::RunLevel()
 		 1.0f, -1.0f,	1.0f, 0.0f,		//4
 		 1.0f,  1.0f,	1.0f, 1.0f 		//5
 	};
-
 	unsigned int quadIndices[] = {
 		0, 1, 2,
 		3, 4, 5,
 	};
+	VertexArray PostProcVAO;
+	PostProcVAO.Bind();
+	VertexBuffer PostProcVBO(quadVertices, 6 * 4 * sizeof(float), kStaticDraw);
+	PostProcVBO.Bind();
+	IndexBuffer PostProcIBO(quadIndices, 6 * 1 * sizeof(unsigned int), kStaticDraw);
+	PostProcIBO.Bind();
+	TempVBL.Push<float>(2, false);
+	TempVBL.Push<float>(2, false);
+	PostProcVBO.SetLayout(TempVBL);
+	TempVBL.Clear();
+	PostProcVAO.AddBuffer(PostProcVBO);
 
-	VertexArray vaoQuad;
-	vaoQuad.Bind();
-
-	VertexBuffer vbQuad(quadVertices, 6 * 4 * sizeof(float), kStaticDraw);
-	vbQuad.Bind();
-
-	VertexBufferLayout vbl;
-
-	IndexBuffer ibQuad(quadIndices, 6 * 1 * sizeof(unsigned int), kStaticDraw);
-	ibQuad.Bind();
+	//Shaders used
+	Shader MainShader("res/shaders/simple_lighting.vert", "res/shaders/simple_lighting.frag");
+	Shader BorderShader("res/shaders/simple_lighting.vert", "res/shaders/border.frag");
+	Shader PostProcessShader("res/shaders/post_process.vert", "res/shaders/post_process.frag");
 	
-	vbl.Push<float>(2, false);
-	vbl.Push<float>(2, false);
-	vbQuad.SetLayout(vbl);
-	vbl.Clear();
-	vaoQuad.AddBuffer(vbQuad);
+	//Models
+	Model VendingMachine("res/objects/vending_machine/untitled.obj");
+	Model Ground("res/objects/try/ground.obj");
 
-	
-	
-	
-	Shader simple_shader("res/shaders/simple_lighting.vert", "res/shaders/simple_lighting.frag");
-	Shader border_shader("res/shaders/simple_lighting.vert", "res/shaders/border.frag");
-	Shader post_process("res/shaders/post_process.vert", "res/shaders/post_process.frag");
-	Shader model_simple_shader("res/shaders/modelsimple.vert", "res/shaders/modelsimple.frag");
-	Model vm("res/objects/vending_machine/untitled.obj");
-
+	//Textures
 	stbi_set_flip_vertically_on_load_thread(1);
-	Texture tex1("res/textures/container.png");
-	Texture tex2("res/textures/container_specular.png");
-
-	tex1.SetType(k2D);
-	tex1.Activate(0);
-	tex1.Bind();
-	tex2.SetType(k2D);
-	tex2.Activate(1);
-	tex2.Bind();
-
-	simple_shader.Use();
-	simple_shader.SetInt("material.texture_diffuse1", 0);
-	simple_shader.SetInt("material.texture_specular1", 1);
-
-
-
-	glm::vec3 offset[] = { glm::vec3(0.0f), glm::vec3(0.0f) };
 	
+	//Timing of last frame initialized
 	m_LastFrame = glfwGetTime();
 	
 	//Post processing getting ready by creating custom frame buffer
@@ -152,9 +130,8 @@ int Game::RunLevel()
 	colorTexInttermediate.SetMinFilter(kLinear);
 	colorTexInttermediate.SetMagFilter(kLinear);
 	colorTexInttermediate.CreateTexImage(m_WindowWidth, m_WindowHeight, kColor);
-	post_process.Use();
-	post_process.SetInt("quadTex", 2);
-	post_process.SetBool("moving", false);
+	PostProcessShader.Use();
+	PostProcessShader.SetInt("quadTex", 2);
 
 	RenderBuffer rboInttermediate;
 	rboInttermediate.Bind();
@@ -167,23 +144,20 @@ int Game::RunLevel()
 	if (!fboInttermediate.IsComplete())
 		return -1;
 
-	int glfwWidth;
-	int glfwHeight;
-	Model ground("res/objects/try/ground.obj");
-
-	m_MovingSpeed = 0;
+	//Setup scene lights	
+	DirectionaLight l0(glm::vec3(0, 0, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), 0.5f, 0.5f, 1.0f, true);
 	PointLight l1(glm::vec3(25, 5, 0), glm::vec3(1.0f, 1.0f, 1.0f), 0.2, 0.8f, 1.0f, 1.0f, 0.00045f, 0.00075f, true);
 	PointLight l2(glm::vec3(50, 5, 30), glm::vec3(1.0f, 1.0f, 1.0f), 0.2, 0.8f, 1.0f, 1.0f, 0.00045f, 0.00075f, true);
-	SpotLight l3(glm::vec3(55, 5, 0), glm::vec3(1, -1, -1), glm::vec3(0.0f, 1.0f, 0.0f), 0.2f, 0.8f, 1.0f, 1.0f, 0.00045f, 0.00075f, glm::radians(55.0f), glm::radians(75.0f), true);
+	SpotLight l3(glm::vec3(75, 5, 0), glm::vec3(1, -1, -1), glm::vec3(0.0f, 1.0f, 0.0f), 0.2f, 0.8f, 1.0f, 1.0f, 0.00045f, 0.00075f, glm::radians(55.0f), glm::radians(75.0f), true);
+	m_LightManager->AddLight(l0);
 	m_LightManager->AddLight(l1);
 	m_LightManager->AddLight(l2);
 	m_LightManager->AddLight(l3);
+
+	//Start of rendering loop
 	while (!glfwWindowShouldClose(m_CurrentWindow))
 	{
-		//If player is moving, apply blur
-		post_process.SetBool("moving", m_Moving);
-		post_process.SetFloat("blurStrength", std::max(m_MovingSpeed,0.0f));
-
+		//Get the window size, and update buffer if they changed
 		glfwGetWindowSize(m_CurrentWindow, &glfwWidth, &glfwHeight);
 		if (glfwWidth != m_WindowWidth || glfwHeight != m_WindowHeight)
 			//Should be done in a better modular way
@@ -198,7 +172,8 @@ int Game::RunLevel()
 			colorTexInttermediate.Bind();
 			colorTexInttermediate.CreateTexImage(m_WindowWidth, m_WindowHeight, kColor);
 		}
-		//Update time
+
+		//Update timings
 		m_CurrentFrame = glfwGetTime();
 		m_DeltaTime = m_CurrentFrame - m_LastFrame;
 		m_LastFrame = m_CurrentFrame;
@@ -210,8 +185,6 @@ int Game::RunLevel()
 		//Render IMGUI GUI
 		ImGui::Begin("Editor");
 		{
-			ImGui::SliderFloat("X offset 1", &offset[0].x, -3.0f, 3.0f);
-			ImGui::SliderFloat("Y offset 1", &offset[0].y, -3.0f, 3.0f);
 			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 		}
 		ImGui::End();
@@ -221,30 +194,25 @@ int Game::RunLevel()
 		m_Renderer.EnableDepthTesting();
 		m_Renderer.Clear(kColorBufferBit | kDepthBufferBit | kStencilBufferBit, glm::vec4(0.4f, 0.2f, 0.4f, 1.0f));
 
-		//Transformations
+		//Transformations for the vending machine
 		glm::mat4 model = glm::mat4(1.0);
 		glm::mat4 view = glm::mat4(1.0);
 		glm::mat4 projection = glm::mat4(1.0);
 		glm::vec3 lightPos = glm::vec3(10 , 10, -5);
 		glm::vec3 lightColor = glm::vec3(0.5+sin(glfwGetTime())/2, 0, 0.5+cos(glfwGetTime())/2);
-		model = glm::translate(model, offset[0]);
 		view = m_Camera.GetViewMatrix();
 		projection = glm::perspective(glm::radians(m_Camera.GetFOV()), (float)m_WindowWidth/(float)m_WindowHeight, 0.1f, 200.0f);
-		simple_shader.Use();
-		simple_shader.SetMat4("model", model);
-		simple_shader.SetMat4("view", view);
-		simple_shader.SetMat4("projection", projection);
-		simple_shader.SetVec3("viewPos", m_Camera.GetPosition());
+		MainShader.Use();
+		MainShader.SetMat4("model", model);
+		MainShader.SetMat4("view", view);
+		MainShader.SetMat4("projection", projection);
+		MainShader.SetVec3("viewPos", m_Camera.GetPosition());
+		MainShader.SetFloat("material.shineness", 32);
 
-		simple_shader.SetFloat("material.shineness", 32);
-
-		//Setup lights
+		//Update lights data
 		l1.SetColor(glm::vec3(std::max<float>(-cos(glfwGetTime() * 2), cos(glfwGetTime() * 2)), 0.0f, std::max<float>(-cos(glfwGetTime() * 3), cos(glfwGetTime() * 3))));
 		l2.SetColor(glm::vec3(std::max<float>(-sin(glfwGetTime() * 2), sin(glfwGetTime()*2)), 0.0f, std::max<float>(-sin(glfwGetTime() * 3), sin(glfwGetTime()*3))));
-		m_LightManager->SetLight(simple_shader);
-
-		
-
+		m_LightManager->SetLight(MainShader);
 
 		//Setup for border (part 1)
 		m_Renderer.EnableStencilTesting();
@@ -252,52 +220,54 @@ int Game::RunLevel()
 		m_Renderer.SetStencilMask(0xff);
 		m_Renderer.SetStencilOp(kKeep, kKeep, kReplace);
 
-		
+
 		//Render using openGL
+		//Render the vending machine
 		model = glm::scale(model, glm::vec3(5.0f));
 		model = glm::translate(model, glm::vec3(9, 0.3f, 0.0f));
 		model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		simple_shader.SetMat4("model", model);
-		vm.Draw(simple_shader);
-		simple_shader.SetMat4("model", model);
+		MainShader.SetMat4("model", model);
+		VendingMachine.Draw(MainShader);
+
+		//Start rendering the Ground but disable border
+		m_Renderer.SetStencilMask(0x00);
+		MainShader.SetMat4("model", model);
 		model = glm::mat4(1.0f);
 		model = glm::translate(model, glm::vec3(0, -5.0f, 0.0f));
 		for (int i = 0; i < 20; i++)
 		{
 			glm::mat4 model2 = model;
-			simple_shader.SetMat4("model", model);
-			ground.Draw(simple_shader);
+			MainShader.SetMat4("model", model);
+			Ground.Draw(MainShader);
 			model = glm::translate(model, glm::vec3(21.f, 0.0f, 0.0f));
 			for (int j = 0; j < 20; j++)
 			{
 				model2 = glm::translate(model2, glm::vec3(0, 0, 17.0f));
-				simple_shader.SetMat4("model", model2);
-				ground.Draw(simple_shader);
+				MainShader.SetMat4("model", model2);
+				Ground.Draw(MainShader);
 			}
 		}
 		
-		//Draw light
-		m_Renderer.SetStencilMask(0x00);
+		//Draw lights (for debug purposes only)
 		m_LightManager->DrawLights(view, projection);
-
 
 		//Skybox
 		m_Skybox->Draw(view, projection);
 
-		//Border for the main cube
+		//Drawing the borders for the vending machine
 		model = glm::mat4(1.0f);
-		model = glm::translate(model, offset[0]);
-		model = glm::scale(model, glm::vec3(1.05f)); //Border is larger
-		border_shader.Use();
-		border_shader.SetMat4("model", model);
-		border_shader.SetMat4("view", view);
-		border_shader.SetMat4("projection", projection);
-		border_shader.SetVec3("BorderColor", glm::vec3(0.4, 0.4, 0.1));
-
-		//m_Renderer.DisableDepthTesting();
+		model = glm::scale(model, glm::vec3(5.0f));
+		model = glm::translate(model, glm::vec3(9, 0.3f, 0.0f));
+		model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(1.03f));	//Border is larger
+		BorderShader.Use();
+		BorderShader.SetMat4("model", model);
+		BorderShader.SetMat4("view", view);
+		BorderShader.SetMat4("projection", projection);
+		BorderShader.SetVec3("BorderColor", glm::vec3(0.9, 0.2, 0.2));
 		m_Renderer.SetStencilFunc(kNotEqual, 1, 0xff);
 		m_Renderer.SetStencilMask(0x00);
-		//m_Renderer.Draw(vao, border_shader, 6 * 6, 0);
+		VendingMachine.Draw(BorderShader);
 		m_Renderer.EnableDepthTesting();
 		m_Renderer.SetStencilMask(0xff);
 		m_Renderer.DisableStencilTesting();
@@ -307,10 +277,13 @@ int Game::RunLevel()
 
 		////Post processing
 		fbo.Unbind();
-		post_process.Use();
-		vaoQuad.Bind();
+		PostProcessShader.Use();
+		//If player is moving, apply blur
+		PostProcessShader.SetBool("moving", m_Moving);
+		PostProcessShader.SetFloat("blurStrength", std::max(m_MovingSpeed, 0.0f));
+		PostProcVAO.Bind();
 		m_Renderer.DisableDepthTesting();
-		m_Renderer.Draw(vaoQuad, post_process, 6 * 1, 0);
+		m_Renderer.Draw(PostProcVAO, PostProcessShader, 6 * 1, 0);
 		
 		//Render GUI onto screen
 		ImGui::Render();
@@ -319,6 +292,7 @@ int Game::RunLevel()
 		glfwSwapBuffers(m_CurrentWindow);
 		glfwPollEvents();
 		ProcessInput();
+		//Update the moving speed
 		m_MovingSpeed = std::max(m_MovingSpeed, -0.5f);
 		m_MovingSpeed = std::min(m_MovingSpeed, 0.8f);
 	}
